5484246ae98c95a3467ea4a9828581eb7de68cad
n3fia <stefania.fraga@gmail.com>
2024-10-12 16:32:00 -0300

    products.js add

---
+++ src/products.js
@@ -0,0 +1,112 @@
+export const products = [
+	{
+		id: "1",
+		title: "Malabrigo",
+		price: 400,
+		stock: 3,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586714/1000x1000_d3lmby.jpg",
+		description: "Malabrigo Rasta Deep",
+		category: "Yarn",
+	},
+	{
+		id: "2",
+		title: "Malabrigo",
+		price: 350,
+		stock: 9,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586742/1000x1000_qrpapf.jpg",
+		description: "Malabrigo Rasta Confetti",
+		category: "Yarn",
+	},
+	{
+		id: "3",
+		title: "Malabrigo",
+		price: 399,
+		stock: 5,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586485/832x832_l3otdd.jpg",
+		description: "Malabrigo Rios Revo",
+		category: "Yarn",
+	},
+	{
+		id: "4",
+		title: "Malabrigo",
+		price: 550,
+		stock: 12,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586521/832x832_ny8aps.jpg",
+		description: "Malabrigo Rios Fire",
+		category: "Yarn",
+	},
+	{
+		id: "5",
+		title: "Revo Swift",
+		price: 2200,
+		stock: 4,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728587756/1000x1000_igtxqn.jpg",
+		description: "Yarn Umbrella Swift",
+		category: "Accessories",
+	},
+	{
+		id: "6",
+		title: "Revo Tape",
+		price: 150,
+		stock: 93,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728587571/1000x1000_cnvm3i.jpg",
+		description: "Measuring tape",
+		category: "Accessories",
+	},
+	{
+		id: "7",
+		title: "Revo Markers",
+		price: 50,
+		stock: 14,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728587685/1000x1000_dbqxak.jpg",
+		description: "Acrilic Stitch Markers",
+		category: "Accessories",
+	},
+	{
+		id: "9",
+		title: "Revo Wood",
+		price: 550,
+		stock: 8,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586960/1000x1000_zpi4ri.jpg",
+		description: "Wood Straight Needles",
+		category: "Needles",
+	},
+	{
+		id: "10",
+		title: "Revo DPNs",
+		price: 600,
+		stock: 15,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728586997/1000x1000_twdoyk.jpg",
+		description: "Double Pointed Needles",
+		category: "Needles",
+	},
+	{
+		id: "11",
+		title: "Revo Socks",
+		price: 5,
+		stock: 999,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728587153/1000x1000_kndkar.jpg",
+		description: "PDF Basic Socks Pattern",
+		category: "Patterns",
+	},
+	{
+		id: "12",
+		title: "Revo Sweater",
+		price: 15,
+		stock: 999,
+		imageUrl:
+			"https://res.cloudinary.com/dhmbftmbi/image/upload/v1728587319/1000x1000_rumhd4.jpg",
+		description: "PDF Everyday Sweater Pattern",
+		category: "Patterns",
+	},
+];
