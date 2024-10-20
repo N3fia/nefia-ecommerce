53e1af6b1875f0ba31915db3450fa336b8deb576
n3fia <stefania.fraga@gmail.com>
2024-10-20 16:32:48 -0300

    un poco de orden y estilo del navbar

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,14 +1,28 @@
+import { products } from "../../../products";
+import Cards from "../../common/cards/Cards";
 import "./itemListContainer.css";
 
-export const ItemListContainer = ({ greeting, intro }) => {
+let myProductsPromise = new Promise((res, rej) => {
+	setTimeout(() => {
+		if (products.length === 0) {
+			rej("No se hay productos");
+		} else {
+			res(products);
+		}
+	}, 2500);
+});
+
+export const ItemListContainer = () => {
+	myProductsPromise.then((data) => {}).catch((err) => {});
 	return (
-		<div className="welcome-message">
-			<h1>{greeting}</h1>
-			<p>{intro}</p>
-			<a href="#store" className="cta-button">
-				Shop Now
-			</a>
-		</div>
+		// <div className="welcome-message">
+		// 	<h1>{greeting}</h1>
+		// 	<p>{intro}</p>
+		// 	<a href="#store" className="cta-button">
+		// 		Shop Now
+		// 	</a>
+		// </div>
+		<Cards />
 	);
 };
 
